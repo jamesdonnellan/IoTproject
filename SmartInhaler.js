fe653629a1e073ea68b31423b772ca36edfcd77e
jamesdonnellan <150438543+jamesdonnellan@users.noreply.github.com>
2025-02-18 13:30:41 +0000

    Add files via upload
    - Currently using w3schools to help me add a curtain menu to navigate my website
    - Added my IoT video to my homepage for further explanation of my project

---
+++ SmartInhaler.js
@@ -0,0 +1,11 @@
+/* Open when span element is clicked */
+function openNav()
+{
+    document.getElementById("testNav").style.height = "100%";
+}
+
+/* Close when button is clicked */
+function closeNav()
+{
+    document.getElementById("testNav").style.height = "0%";
+}
